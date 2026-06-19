ba07127b49aa65b231444a23916ca929158bb945
kwasii1 <kesbijnr@gmail.com>
2026-06-19 17:36:04 +0000

    sonner working

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,2 @@
-(() => {
-    let registered = false
 
-    document.addEventListener('livewire:init', () => {
-        if (registered) return
-        registered = true
 
-        Livewire.on('toast', (params) => {
-            console.trace('[bridge] Livewire toast → CustomEvent', JSON.stringify(params))
-            window.dispatchEvent(new CustomEvent('toast', { detail: params }))
-        })
-    })
-})()
