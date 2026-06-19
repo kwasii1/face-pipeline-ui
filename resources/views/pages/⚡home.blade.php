2c9d3c265148e9e9ab080f88f4c346c6cf440a14
kwasii1 <kesbijnr@gmail.com>
2026-06-19 14:06:40 +0000

    first commit

---
+++ resources/views/pages/⚡home.blade.php
@@ -0,0 +1,18 @@
+<?php
+
+use Livewire\Component;
+use Livewire\Attributes\Layout;
+
+new
+#[Layout('layouts::app')]
+class extends Component
+{
+    //
+};
+?>
+
+<div class="flex h-screen items-center justify-center">
+    <div class="flex p-5 bg-white">
+
+    </div>
+</div>
